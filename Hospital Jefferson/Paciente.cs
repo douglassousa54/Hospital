6d7295ccbfa51f1d14991df674b1ed38b8abfe2f
unknown <you@example.com>
2022-04-27 22:12:34 -0300

    criado ate primeiro paciente

---
+++ Hospital Jefferson/Paciente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Jefferson
+{
+    class Paciente
+    {
+        int idade;
+        string nome;
+        string CPF;
+
+        public void cadastro ()
+        {
+            Console.WriteLine("Digite o nome do paciente");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite o CPF do paciente");
+            CPF = Console.ReadLine();
+            Console.WriteLine("Digite a idade do paciente");
+            idade = int.Parse(Console.ReadLine());
+        }
+
+        }
+
+    
+}
---
+++ Hospital Jefferson/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Jefferson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Paciente[] cadastro1 = new Paciente[10];
+
+            Console.WriteLine("MENU");
+            Console.WriteLine("Cadastrar Pacientes - C");
+            Console.WriteLine("Listar Pacientes - L");
+            Console.WriteLine("Atender um paciente - A");
+            Console.WriteLine("Alterar dados do paciente - D");
+            Console.WriteLine("Sair do programa - Q");
+            string escolha = Console.ReadLine();
+
+            switch (escolha)
+            {
+                case "C":
+                    
+                    Paciente exemplo = new Paciente();
+                    exemplo.cadastro();
+                    cadastro1[i] = exemplo;
+
+                    break;
+
+                case "L":
+
+                    break;
+
+                case "A":
+
+                    break;
+
+                case "D":
+
+                    break;
+
+                case "Q":
+
+                    break;
+                
+            }
+        }
+    }
+}
